07444e11c770e4c8539264d35d5d91107f5d6336
hunggahot <hunggahot@gmail.com>
2023-07-08 18:04:20 +0700

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Footer.scss";
 
+import { currentYear } from "../../utils/times";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <footer className="footer py-4 bg-purple">
+      <div className="container">
+        <div className="footer-content text-white text-center">
+          <p className="copyright-text font-rubik fw-4 ls-1">
+            &copy; {currentYear} CopyRight Example. All rights reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
