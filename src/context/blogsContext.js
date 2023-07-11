675e5f7af864aadf867c1f4bdd8877fbac0e9f71
hunggahot <hunggahot@gmail.com>
2023-07-11 09:46:07 +0700

    single blog page

--- src/context/blogsContext.js
+++ src/context/blogsContext.js
@@ -44,7 +44,16 @@ export const BlogsProvider = ({ children }) => {
     }
   };
 
-  const fetchSingleBlog = async (id) => {};
+  const fetchSingleBlog = async (id) => {
+    dispatch({ type: GET_SINGLE_BLOG_BEGIN });
+    try {
+      const response = await axios.get(`${BLOG_URL}/${id}`);
+      dispatch({ type: GET_SINGLE_BLOG_SUCCESS, payload: response.data });
+    } catch (err) {
+      console.log(err);
+      dispatch({ type: GET_SINGLE_BLOG_ERROR });
+    }
+  };
 
   const fetchBlogsFromSearch = async (searchTerm) => {
     dispatch({ type: GET_BLOG_BY_SEARCHTERM_BEGIN });
--- src/pages/SingleBlogPage/SingleBlogPage.js
+++ src/pages/SingleBlogPage/SingleBlogPage.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SingleBlogPage.scss";
+import Title from "../../components/Title/Title";
+import SingleBlog from "../../components/SingleBlog/SingleBlog";
+import { useBlogsContext } from "../../context/blogsContext";
+import { useParams } from "react-router-dom";
+import { banner_image } from "../../utils/images";
 
 const SingleBlogPage = () => {
-  return <></>;
+  const { fetchSingleBlog, singleBlog } = useBlogsContext();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchSingleBlog(id);
+  }, [singleBlog.id, id]);
+
+  console.log(singleBlog);
+
+  return (
+    <div className="main-holder bg-light-blue">
+      <header
+        className="header"
+        style={{
+          background: `linear-gradiem(rgba(0,0,0,0.1), rgba(0,0,0,0.2)), url(${banner_image}) center/cover no-repeat`,
+        }}
+      >
+        <div className="container">
+          <div className="header-content text-center flex align-center justify-center flex-column text-white">
+            <Title title="Blog Details" color={`#fff`} />
+          </div>
+        </div>
+      </header>
+
+      <section className="section py-7">
+        <div className="container">
+          <div className="section-content bg-white">
+            <SingleBlog blog={singleBlog} />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default SingleBlogPage;
